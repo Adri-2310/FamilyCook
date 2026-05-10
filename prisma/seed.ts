03505497a035c9a3f83214b960b9c2ee251ceeb2
Adrien Mertens <adrien231020@gmail.com>
2026-05-10 17:38:16 +0200

    Configurer les images placeholder et le seed
    - Ajouter via.placeholder.com à la liste des domaines autorisés pour les images
    - Configurer seed avec ts-node et dotenv pour charger les variables d'environnement

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,8 @@
 import { PrismaClient, Category, Difficulty, Visibility } from "@prisma/client";
 import * as bcrypt from "bcrypt";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
 
 const prisma = new PrismaClient();
 
